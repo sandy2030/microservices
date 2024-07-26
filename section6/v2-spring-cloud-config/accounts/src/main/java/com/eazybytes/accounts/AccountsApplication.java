bd2dd4e2f9286290319c8504db09ecdc8631863a
sandy2030 <sandeeppandey2024@gmail.com>
2024-07-26 11:44:07 +0530

    added changes

---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,44 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.SpringVersion;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API documentation",
+				description = "My Bank Accounts microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Sandeep Pandey",
+						email = "sandip.cimb@gmail.com", // team group mail  id
+						url = "https://mywebsite.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://mywebsite.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "My Bank Accounts microservice REST API external documentation",
+				url = "https://mywebsite.com"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/constants/AccountsConstants.java
@@ -0,0 +1,19 @@
+package com.eazybytes.accounts.constants;
+
+public class AccountsConstants {
+    private AccountsConstants() {
+    }
+    public static final String SAVINGS="Savings";
+    public static final String ADDRESS="007 ISB Road, Hyderabad";
+    public static final String STATUS_201="201";
+    public static final String STATUS_200="200";
+    public static final String MESSAGE_200="Request processed Successfully";
+    public static final String MESSAGE_201="Account Created Successfully";
+    public static final String STATUS_500="500";
+    public static final String MESSAGE_500="An error occurred.Please try again or Contact Dev Team";
+
+    public static final String STATUS_417="417";
+    public static final String MESSAGE_417_UPDATE="Update Operation failed. Please try again or contact Dev Team";
+    public static final String MESSAGE_417_DELETE="Delete Operation failed. Please try again or contact Dev Team";
+
+}
